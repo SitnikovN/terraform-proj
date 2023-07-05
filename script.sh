21ae1342b27d3408bf33668ae6c235238528cabb
Nikita Sitnikov <nikita.sitnikov@agmatix.com>
2023-07-05 17:51:14 +0200

    script.sh wip14

--- script.sh
+++ script.sh
@@ -6,6 +6,8 @@ funcs[1]="lambda_func_2"
 
 commit_message=$(git diff-tree --name-only -r --no-commit-id HEAD)
 echo $commit_message
+set +e
+
 
 for func in "${funcs[@]}"; do
   if [[ $commit_message =~ func ]]; then
@@ -14,7 +16,8 @@ for func in "${funcs[@]}"; do
           $(aws ecr get-login --no-include-email --region eu-central-1)
           docker tag lambda-func-1 764717859735.dkr.ecr.eu-central-1.amazonaws.com/lambda-func-1:latest
           docker push 764717859735.dkr.ecr.eu-central-1.amazonaws.com/lambda-func-1:latest
-          aws lambda update-function-code --function-name lambda-func-1 --image-uri 764717859735.dkr.ecr.eu-central-1.amazonaws.com/lambda-func-1:latest
+          res=$(aws lambda update-function-code --function-name lambda-func-1 --image-uri 764717859735.dkr.ecr.eu-central-1.amazonaws.com/lambda-func-1:latest)
+          echo $res
     fi
 done
 
